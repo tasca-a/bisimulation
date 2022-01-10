49f5a70d691b016b5b2916f1fe8d0e713926a92b
Alessandro Tasca <a.tasca@campus.unimib.it>
2022-01-10 13:50:24 +0100

    Added password recovery functionality.

--- app/src/main/java/com/example/bisimulation/login/LoginActivity.kt
+++ app/src/main/java/com/example/bisimulation/login/LoginActivity.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.bisimulation.R
 import com.example.bisimulation.databinding.ActivityLoginBinding
 import com.example.bisimulation.signup.SignUpActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var viewModel: LoginViewModel
@@ -36,18 +39,52 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
 
-        // "Remember me" functionality
-        val sharedPreferences = this.getSharedPreferences("com.example.bisimulation", Context.MODE_PRIVATE)
-        binding.rememberMeCheckBox.isChecked = sharedPreferences.getBoolean("rememberMe", false)
-        viewModel.email = sharedPreferences.getString("email", "")!!
-        viewModel.password = sharedPreferences.getString("password", "")!!
-        binding.rememberMeCheckBox.setOnClickListener { setPreferences(sharedPreferences) }
+        // Forgot-password handling
+        binding.clickHereTextView.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(this)
+            bottomSheetDialog.setContentView(R.layout.dialog_forgot_password)
+            bottomSheetDialog.setCanceledOnTouchOutside(true)
+
+            val email = bottomSheetDialog.findViewById<EditText>(R.id.emailForgotPwDialog_editText)
+            val reset = bottomSheetDialog.findViewById<Button>(R.id.resetForgotPwDialog_button)
+
+            email?.text = binding.emailEditText.text
+
+            reset?.setOnClickListener {
+                if (email != null) {
+                    if (isEmail(email.text)) {
+                        viewModel.resetPassword(email.text.toString())
+                        bottomSheetDialog.dismiss()
+                    }
+                    else
+                        email.error = resources.getString(R.string.invalidEmailError)
+                }
+            }
+
+            bottomSheetDialog.show()
+        }
+        viewModel.resetPasswordStatus.observe(this){ sent ->
+            val message: String
+            if (sent)
+                message = resources.getString(R.string.forgotPwDialogSuccess_Toast)
+            else
+                message = resources.getString(R.string.forgotPwDialogFail_Toast)
+
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
 
         // Sign-up handling
         binding.registerClickHereTextView.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        // "Remember me" functionality
+        val sharedPreferences = this.getSharedPreferences("com.example.bisimulation", Context.MODE_PRIVATE)
+        binding.rememberMeCheckBox.isChecked = sharedPreferences.getBoolean("rememberMe", false)
+        viewModel.email = sharedPreferences.getString("email", "")!!
+        viewModel.password = sharedPreferences.getString("password", "")!!
+        binding.rememberMeCheckBox.setOnClickListener { setPreferences(sharedPreferences) }
 
         setContentView(binding.root)
     }
--- app/src/main/java/com/example/bisimulation/login/LoginViewModel.kt
+++ app/src/main/java/com/example/bisimulation/login/LoginViewModel.kt
@@ -1,10 +1,8 @@
 package com.example.bisimulation.login
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.bisimulation.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -18,22 +16,16 @@ class LoginViewModel : ViewModel() {
     private var _logInStatus = MutableLiveData<Boolean>()
     val logInStatus: LiveData<Boolean> = _logInStatus
     fun logIn(){
-        Log.i("LoginViewModel", "Sign in triggered.")
-
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            if (task.isSuccessful){
-                Log.d(TAG, "Log In successful.")
-                _logInStatus.value = true
-            }
-            else {
-                Log.e(TAG, "Log In failed.")
-                _logInStatus.value = false
-            }
+            _logInStatus.value = task.isSuccessful
         }
     }
 
-    companion object{
-        private const val TAG = "LogInViewModel"
+    private var _resetPasswordStatus = MutableLiveData<Boolean>()
+    val resetPasswordStatus: LiveData<Boolean> = _resetPasswordStatus
+    fun resetPassword(emailToReset: String){
+        auth.sendPasswordResetEmail(emailToReset).addOnCompleteListener { task ->
+            _resetPasswordStatus.value = task.isSuccessful
+        }
     }
-
 }
