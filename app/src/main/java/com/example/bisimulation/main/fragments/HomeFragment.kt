4a2c38c9db4d128bcee9a3a219d4004f7d5d62f2
Alessandro Tasca <a.tasca@campus.unimib.it>
2022-02-07 15:34:55 +0100

    Added share functionality.

--- app/src/main/java/com/example/bisimulation/main/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/bisimulation/main/fragments/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.bisimulation.main.fragments
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.bisimulation.R
 import com.example.bisimulation.databinding.FragmentHomeBinding
 import com.example.bisimulation.main.SharedViewModel
 
@@ -21,9 +24,41 @@ class HomeFragment : Fragment() {
     ): View {
         homeFragmentSetup(inflater, container)
 
+        //Discover more button redirects to the Profile page
         binding.discoverMoreButton.setOnClickListener {
             val action = HomeFragmentDirections.actionHomeToProfile()
             findNavController().navigate(action)
+        }
+
+        //Clicking on the Share fab created a dialog that let you choose if you want to share or not
+        binding.shareFab.setOnClickListener {
+            //Create the dialog
+            val alertDialog: AlertDialog? = activity?.let {
+                val builder = AlertDialog.Builder(it)
+                builder.apply {
+                    setPositiveButton(R.string.shareDialogPositiveButton) { _, _ ->
+                        val sendIntent: Intent = Intent().apply {
+                            action = Intent.ACTION_SEND
+                            putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.shareMessage))
+                            type = "text/plain"
+                        }
+
+                        val shareIntent = Intent.createChooser(sendIntent, null)
+                        startActivity(shareIntent)
+                    }
+                    setNegativeButton(R.string.shareDialogNegativeButton) { dialogInterface, _ ->
+                        dialogInterface.dismiss()
+                    }
+                }
+
+                builder.setTitle(R.string.shareDialogTitle)
+                builder.setMessage(R.string.shareDialogMessage)
+
+                builder.create()
+            }
+
+            //Show the dialog
+            alertDialog?.show()
         }
 
         return binding.root
